ccb7fe6c4f56a6a59f7a0bf4ca4e2fc6e8e127a8
Hassan <hasanfarahani97@gmail.com>
2020-01-01 01:15:48 -0600

    Add point

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+   Vector3 point;
+
+   [SerializeField] float size = 0.1f;
+
+   Camera camera;
+
+   void Awake() {
+      camera = Camera.main;
+   }
+   void Start() {
+      float scale = Vector3.Distance(camera.transform.position, transform.position) * size;
+      transform.localScale = new Vector3(scale, scale, scale);
+   }
+
+   void Update() {
+      float scale = Vector3.Distance(camera.transform.position, transform.position) * size;
+
+      transform.localScale = new Vector3(scale, scale, scale);
+   }
+}
